fba74a602d9d0d8b10e12fecd87385c717bcac73
cyberart <xmachin3x@yahoo.co.id>
2014-03-05 22:42:08 +0700

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,9 +3,6 @@
 # go to root
 cd
 
-# set time GMT +7
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-
 # set locale
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service sshd restart
@@ -28,14 +25,6 @@ yum -y remove cyrus-sasl
 # update
 yum -y update
 
-# install essential package
-yum -y install iftop htop nmap bc nethogs openvpn vnstat ngrep mtr git zsh mrtg unrar rsyslog rkhunter mrtg net-snmp net-snmp-utils expect nano bind-utils
-yum -y groupinstall 'Development Tools'
-yum -y install cmake
-
-# matiin exim
-service exim stop
-chkconfig exim off
 
 # setting port ssh
 echo "Port 143" >> /etc/ssh/sshd_config
@@ -50,17 +39,10 @@ echo "/bin/false" >> /etc/shells
 service dropbear restart
 chkconfig dropbear on
 
-# install fail2ban
-yum -y install fail2ban
-service fail2ban restart
-chkconfig fail2ban on
 
 # install squid
 yum -y install squid
-wget -O /etc/squid/squid.conf "https://raw.github.com/arieonline/autoscript/master/conf/squid-centos.conf"
-sed -i $MYIP2 /etc/squid/squid.conf;
-service squid restart
-chkconfig squid on
+
 
 # install webmin
 cd
@@ -76,16 +58,9 @@ cd
 wget "https://raw.github.com/cyber4rt/installer/master/loginview.sh"
 sed -i 's/auth.log/secure/g' loginview.sh
 chmod +x loginview.sh
-# cron
-service crond start
-chkconfig crond on
-
-# finalisasi
-chown -R nginx:nginx /home/vps/public_html
 
 service sshd restart
 service dropbear restart
-service fail2ban restart
 service squid restart
 service webmin restart
 
@@ -112,7 +87,6 @@ echo "Fitur lain"
 echo "----------"
 echo "Webmin   : http://$MYIP:10000/"
 echo "Timezone : Asia/Jakarta"
-echo "Fail2Ban : [on]"
 echo ""
 echo "SILAHKAN REBOOT VPS ANDA !"
 echo ""
